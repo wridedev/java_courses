8ff5da6e6723171703ffd7468b52cb9db42251e5
wridedev <wridedev@gmail.com>
2018-06-08 17:50:12 +0300

    add math func

--- Calculate.java
+++ Calculate.java
@@ -2,6 +2,21 @@ public class Calculate{
 	
 	public static void main(String[] arg)	{
 		System.out.println("Calculate...");
+		int first = Integer.valueOf(arg[0]);
+		String zn = arg[1];
+		int second = Integer.valueOf(arg[2]);
+		int summ=0;
+
+		
+		if(zn.equals("+")) summ = first + second;
+		if(zn.equals("-")) summ = first - second;
+		if(zn.equals("*")) summ = first * second;
+		if(zn.equals("/")) summ = first / second;
+		if(zn.equals("%")) summ = first % second;
+		
+		if(summ == 0) System.out.println("ne pravilniy mat. znak");
+		else
+		System.out.println("Sum = " + summ + zn);
 		
 	}
 }
